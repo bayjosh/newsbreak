62ef3040408f521e55f6659050b43cda3a5871ec
Josh Bay <joshbay@dhcp-10-105-138-188.wireless.northwestern.private>
2018-02-23 11:32:46 -0600

    reorganized files

--- assets/javascript/ajax.js
+++ assets/javascript/ajax.js
@@ -1,103 +1,171 @@
-//THE NEW YORK TIMES
-
-var nytQueryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=465a473b34ef4dd4a0ee8c44278471a2&q=obama";
-
-var nytTotalReadTime = 0;
-
-function getNYTData(results) {
-
-    var nytWordcount = results.word_count;
-
-    // console.log("nytWordcount: " + nytWordcount);
-
-    var nytReadTime = Math.round(nytWordcount / 250);
-
-    // console.log("nytReadTime: " + nytReadTime);
-
-    var nytHeadline = results.headline.main;
-    // console.log(nytHeadline);
-
-    var nytLink = results.web_url;
-
-    return {
-        wordCount:nytWordcount,
-        readTime: nytReadTime,
-        headline: nytHeadline,
-        link: nytLink,
-    }
-}
-
-function addNYTArticles(nyt) {
-
-    var nytArticleText = $("<div>").attr("class", "nytSection");
-
-    var nytHeadlineText = $("<p>").html("<a href='" + nyt.link + "' target='_blank'>" + nyt.headline + "</a>");
-
-    var nytReadTimeText =
-        $("<p>").text("Estimated read time: " + nyt.readTime + " min");
-
-    nytArticleText.append(nytHeadlineText, nytReadTimeText);
-
-    $("#nyt").append(nytArticleText);
-}
+//Create initial array of topics
+var topic = ["Obama", "Trump", "Olympics"];
 
 
-function displayNYTArticles(input) {
+$(document).ready(function () {
 
-    var nytResults = input.response.docs;
 
-    for (var i = 0; i < nytResults.length; i++) {
+    //Event listener that pushes user input into array and creates an associated button
 
-        var nyt = getNYTData(nytResults[i]);
+    $("body").on("click", "#add-topic", function (event) {
+        event.preventDefault();
 
-        console.log(nyt);
+        var topicInput = $("#topic-input").val().trim();
 
-        nytTotalReadTime += nyt.readTime;
+        if (topicInput != "") {
 
-        if (nyt.readTime > 1 && nyt.readTime < 10 && nytTotalReadTime <= 25) {
+            topic.push(topicInput);
 
-            addNYTArticles(nyt);
+            $("#topic-input").val("");
+
+            renderButtons();
 
         } else {
-            nytTotalReadTime -= nyt.readTime
+            // Get the modal
+            var modal = document.getElementById('myModal');
+
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+
+            modal.style.display = "block";
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function () {
+                modal.style.display = "none";
+            }
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function (event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
         }
 
-        // console.log("Total NYT read time =" + nytTotalReadTime)
-        // console.log("NYT index: " + nytIndex);
-    }
+    });
 
-}
+    //Puts buttons on the page from the array of topics
+    function renderButtons() {
 
-$.ajax({
-    url: nytQueryURL,
-    method: 'GET'
-}).then(function (input) {
+        $("#buttons-view").empty();
 
-    displayNYTArticles(input);
-    $("#nyt").append("Total read time: " + nytTotalReadTime + "min");
+        for (var i = 0; i < topic.length; i++) {
+
+            var btn = $("<button>");
+
+            btn.addClass("topic-btn waves-effect waves-light btn-large");
+
+            btn.attr("data-topic", topic[i]);
+
+            btn.text(topic[i]);
+
+            $("#buttons-view").append(btn);
+        };
+    };
+
+    //Calls the function
+    renderButtons();
+
+
 
 });
 
 
 
+//Event listener to trigger the AJAX request
+$("body").on("click", ".topic-btn", function () {
+
+    var choiceTopic = $(this).attr("data-topic");
+
+    var nytQueryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=465a473b34ef4dd4a0ee8c44278471a2&q=" + choiceTopic;
+
+    var nytTotalReadTime = 0;
+
+    var guardQueryURL = "https://content.guardianapis.com/search?q=" + choiceTopic + "&show-fields=all&page-size=200&api-key=ee30fe53-cc69-4403-802d-998ba44e8fa7";
+
+    var guardTotalReadTime = 0;
+
+  
+    $("#nyt").empty();
+    $("#guardian").empty();
 
 
-//THE GUARDIAN
+    function getNYTData(results) {
 
-var guardQueryURL = "https://content.guardianapis.com/search?q=trump&show-fields=all&page-size=200&api-key=ee30fe53-cc69-4403-802d-998ba44e8fa7";
+        var nytWordcount = results.word_count;
 
-var guardTotalReadTime = 0;
+        var nytReadTime = Math.round(nytWordcount / 275);
+
+        var nytHeadline = results.headline.main;
+
+        var nytLink = results.web_url;
+
+        return {
+            wordCount: nytWordcount,
+            readTime: nytReadTime,
+            headline: nytHeadline,
+            link: nytLink,
+        }
+    }
+
+    function addNYTArticles(nyt) {
+
+        var nytArticleText = $("<div>").attr("class", "nytSection");
+
+        var nytHeadlineText = $("<p>").html("<a href='" + nyt.link + "' target='_blank'>" + nyt.headline + "</a>");
+
+        var nytReadTimeText =
+            $("<p>").text("Estimated read time: " + nyt.readTime + " min");
+
+        nytArticleText.append(nytHeadlineText, nytReadTimeText);
+
+        $("#nyt").append(nytArticleText);
+
+    }
+
+
+    function displayNYTArticles(input) {
+
+        var nytResults = input.response.docs;
+
+        for (var i = 0; i < nytResults.length; i++) {
+
+            var nyt = getNYTData(nytResults[i]);
+
+            nytTotalReadTime += nyt.readTime;
+
+            if (nyt.readTime > 1 && nyt.readTime < 10 && nytTotalReadTime <= 10) {
+
+                addNYTArticles(nyt);
+
+            } else {
+                nytTotalReadTime -= nyt.readTime
+            }
+        }
+
+    }
+
+    $.ajax({
+        url: nytQueryURL,
+        method: 'GET'
+    }).then(function (input) {
+
+        var nytHeader = $('<h4 class="teal lighten-2">').text("The New York Times Articles")
+        $("#nyt").prepend(nytHeader);
+        displayNYTArticles(input);
+        $("#nyt").append("Total read time: " + nytTotalReadTime + "min");
+
+    });
+
+
 
 function getGuardData(results) {
 
     var guardWordcount = results.fields.wordcount;
-    console.log(guardWordcount);
 
-    var guardReadTime = Math.round(guardWordcount / 250);
-    // console.log (guardReadTime);
+    var guardReadTime = Math.round(guardWordcount / 275);
 
     var guardHeadline = results.webTitle;
-    // console.log(guardHeadline);
 
     var guardLink = results.webUrl;
 
@@ -116,18 +184,13 @@ function addGuardArticles(guard) {
 
     var guardHeadlineText = $("<p>").html("<a href='" + guard.link + "' target='_blank'>" + guard.headline + "</a>");
 
-    // console.log("headline + link =" + guardHeadlineText)
-
     var guardReadTimeText =
         $("<p>").text("Estimated read time: " + guard.readTime + " min");
 
     guardArticleText.append(guardHeadlineText, guardReadTimeText);
 
     $("#guardian").append(guardArticleText);
-
-    // console.log("Total read time =" + guardTotalReadTime)
-    // console.log("Guard index: " + guardIndex);
-
+               
 }
 
 function displayGuardArticles(input) {
@@ -138,15 +201,9 @@ function displayGuardArticles(input) {
 
         var guard = getGuardData(guardResults[i]);
 
-        console.log(guardResults[i]);
-
-        console.log(guard.readTime);
-
-        console.log(guard);
-
         guardTotalReadTime += guard.readTime;
 
-        if (guard.readTime > 5 && guard.readTime < 10 && guardTotalReadTime <= 25) {
+        if (guard.readTime > 5 && guard.readTime < 10 && guardTotalReadTime <= 10) {
 
             addGuardArticles(guard);
 
@@ -163,7 +220,22 @@ $.ajax({
     method: 'GET'
 }).then(function (input) {
 
+    var guardHeader = $('<h4 class="teal lighten-2">').text("The Guardian's Articles")
+    $("#guardian").prepend(guardHeader);
     displayGuardArticles(input);
     $("#guardian").append("Total read time: " + guardTotalReadTime + "min");
+
+        var button = document.createElement("button");
+    button.innerHTML = "Finished Reading?";
+
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(button);
+
+    button.addEventListener ("click", function() {
+     
+            window.location='break.html';
+    });
+
+});
 
 });
